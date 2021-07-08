eaf2daeef6acfef1608b7d619a9481037ab956d5
sakshidhamne <72662212+sakshidhamne@users.noreply.github.com>
2021-07-08 14:28:25 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,8 @@
+#add functiondef add(x,y):
+    return x+y#subtract function
+def subtract(x,y):
+    pass#multiply function
+def multiply(x,y):
+    return x*y#divide function
+def divide(x,y):
+    pass
